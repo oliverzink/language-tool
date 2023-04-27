cd7f2205ee65d64bbb296d0b51647be6e45a4a97
Oliver Zink <oliverzink@Olivers-MacBook-Air.local>
2023-04-26 21:41:14 -0400

    added missing file

---
+++ popup.js
@@ -0,0 +1,9 @@
+// document.addEventListener('DOMContentLoaded', function(){
+//     var checkPageButton = document.getElementById("button1");
+//     checkPageButton.addEventListener('click', function(){
+//         chrome.tabs.getSelected(null, function(tab){
+//             alert("YO")
+//         });
+//     }, false);
+// });
+// });
