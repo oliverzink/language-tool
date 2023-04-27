9e97746406c1fb61d1e6a547545a1f03102076e1
Oliver Zink <98194848+oliverzink@users.noreply.github.com>
2023-04-26 21:55:14 -0400

    Delete popup.js
    deleting irrelevant file

--- popup.js
+++
@@ -1,9 +0,0 @@
-// document.addEventListener('DOMContentLoaded', function(){
-//     var checkPageButton = document.getElementById("button1");
-//     checkPageButton.addEventListener('click', function(){
-//         chrome.tabs.getSelected(null, function(tab){
-//             alert("YO")
-//         });
-//     }, false);
-// });
-// });
